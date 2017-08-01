0c1b32db28574870e2fe44a6287dbb594ca4e9a8
Guillaume Giamarchi <guillaume.giamarchi@gmail.com>
2017-08-01 17:38:01 +0200

    Add model for PXE hosts

---
+++ model/host.go
@@ -0,0 +1,31 @@
+package model
+
+import "fmt"
+
+type Host struct {
+	Name          string         `json:"name" yaml:"name"`
+	MACAddresses  []string       `json:"macAddresses" yaml:"mac_addresses"`
+	Configuration *Configuration `json:"configuration"`
+}
+
+func (h *Host) String() string {
+	return fmt.Sprintf("%+v", *h)
+}
+
+type HostQuery struct {
+	Name          string `json:"name"`
+	MACAddress    string `json:"macAddress"`
+	Configuration string `json:"configuration"`
+}
+
+func (h *HostQuery) String() string {
+	return fmt.Sprintf("%+v", *h)
+}
+
+type HostsQuery struct {
+	Hosts []*HostQuery `json:"hosts"`
+}
+
+func (h *HostsQuery) String() string {
+	return fmt.Sprintf("%+v", *h)
+}
