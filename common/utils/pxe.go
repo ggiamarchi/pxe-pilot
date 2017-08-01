24c7f7889df946c4d45f272c5ce64bf162710597
Guillaume Giamarchi <guillaume.giamarchi@gmail.com>
2017-08-01 14:55:53 +0200

    Utilities functions for PXE

---
+++ common/utils/pxe.go
@@ -0,0 +1,16 @@
+package utils
+
+import (
+	"fmt"
+	"strings"
+
+	"dev.splitted-desktop.com/horizon/pxe-pilot/model"
+)
+
+func PXEFilenameFromMAC(mac string) string {
+	return strings.ToLower(fmt.Sprintf("01-%s-%s-%s-%s-%s-%s", mac[0:2], mac[3:5], mac[6:8], mac[9:11], mac[12:14], mac[15:17]))
+}
+
+func PXEFilePathFromMAC(appConfig *model.AppConfig, mac string) string {
+	return fmt.Sprintf("%s/pxelinux.cfg/%s", appConfig.Tftp.Root, PXEFilenameFromMAC(mac))
+}
